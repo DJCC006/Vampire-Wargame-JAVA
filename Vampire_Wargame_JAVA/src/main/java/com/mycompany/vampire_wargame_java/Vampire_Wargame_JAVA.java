362c09e491aef08c360d89d037f5de2f52a83856
DJCC <david.carcamo2006@gmail.com>
2025-10-16 10:38:40 -0600

    creacion de projecto

---
+++ Vampire_Wargame_JAVA/src/main/java/com/mycompany/vampire_wargame_java/Vampire_Wargame_JAVA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vampire_wargame_java;
+
+/**
+ *
+ * @author David
+ */
+public class Vampire_Wargame_JAVA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
