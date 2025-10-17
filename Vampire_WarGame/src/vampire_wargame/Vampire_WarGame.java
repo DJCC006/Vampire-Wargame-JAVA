e30f2f8bcc0602c77ae272ec9298f8213939c699
DJCC <david.carcamo2006@gmail.com>
2025-10-17 16:54:20 -0600

    correccion de tipo de proyecto

---
+++ Vampire_WarGame/src/vampire_wargame/Vampire_WarGame.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vampire_wargame;
+
+/**
+ *
+ * @author David
+ */
+public class Vampire_WarGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
