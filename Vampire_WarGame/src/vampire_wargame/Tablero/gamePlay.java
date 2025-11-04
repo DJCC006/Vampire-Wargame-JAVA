80b0782dc5327d97e6e8add5ce47ca99a628b2da
DJCC <david.carcamo2006@gmail.com>
2025-11-03 18:56:39 -0600

    Desarrollo de bosquejo UI para ventana de juego

--- Vampire_WarGame/src/vampire_wargame/Tablero/gamePlay.java
+++ Vampire_WarGame/src/vampire_wargame/Tablero/gamePlay.java
@@ -5,7 +5,10 @@
 package vampire_wargame.Tablero;
 
 import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
@@ -18,12 +21,13 @@ public class gamePlay {
     public gamePlay(){
         SwingUtilities.invokeLater(()-> {
             JFrame screen = new JFrame();
-            screen.setSize(2000, 900);  //Tamaño standard para menus
+            screen.setSize(3000, 900);  //Tamaño standard para menus
             screen.setResizable(false);
             screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             screen.setLocationRelativeTo(null);
             screen.setLayout(null);
 
+            //CONFIG DE RULETA
             JPanel ruletaPanel = new JPanel();
             ruletaGen generadorRuleta= new ruletaGen(ruletaPanel);
             
@@ -41,10 +45,40 @@ public class gamePlay {
             ruletaPanel.setLayout(null);
             ruletaPanel.add(generadorRuleta);
             ruletaPanel.setBounds(60, 10, 350, 400);
-            //ruletaPanel.setBackground(Color.red);
             
+            JPanel panelAnuncios = new JPanel();
+            panelAnuncios.setBounds(500, 650, 600, 100);
+            panelAnuncios.setBackground(Color.GRAY);
+            
+            //DISPLAYERS DE PLAYERS
+            JPanel cementerioPlayer = new JPanel();
+            cementerioPlayer.setBounds(1140, 100, 350, 300);
+            cementerioPlayer.setBackground(Color.GRAY);
+            
+            JLabel gravePlayerLB= new JLabel("CEMENTERIO PLAYER");
+            gravePlayerLB.setBounds(1140, 30, 350, 80);
+            gravePlayerLB.setFont(new Font("Serif", Font.BOLD, 30));
 
-
+            
+            JPanel cementerioContricante = new JPanel();
+            cementerioContricante.setBounds(1140, 500, 350, 300);
+            cementerioContricante.setBackground(Color.GRAY);
+            
+            JLabel graveContriLB= new JLabel("CEMENTERIO CONTRICANTE");
+            graveContriLB.setBounds(1140, 440, 350, 80);
+            graveContriLB.setFont(new Font("Serif", Font.BOLD, 30));
+            
+            
+            
+            JButton rendirseBT= new JButton("RENDIRSE");
+            rendirseBT.setBounds(60, 500, 300, 80);
+            
+            screen.add(rendirseBT);
+            screen.add(cementerioPlayer);
+            screen.add(gravePlayerLB);
+            screen.add(graveContriLB);
+            screen.add(cementerioContricante);
+            screen.add(panelAnuncios);
             screen.add(ruletaPanel);
             screen.add(panelTablero);
             screen.setVisible(true);
